a16331f8155cd1b50b772a045a8594eb1790cf07
Fredrik Källström <fredrik.kallstrom@learnifier.com>
2024-12-25 12:52:50 +0100

    initial setup

---
+++ nuxt.config.ts
@@ -0,0 +1,24 @@
+import { defineNuxtConfig } from "nuxt/config";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ["@nuxtjs/tailwindcss", "@nuxt/eslint"],
+
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.BACKEND_URL || "http://localhost:8080", // Your backend URL
+    },
+    authSecret: process.env.NUXT_API_SECRET,
+    authOrigin: process.env.NUXT_API_ORIGIN,
+  },
+  css: ["~/assets/css/tailwind.css", "~/assets/css/theme.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+});
